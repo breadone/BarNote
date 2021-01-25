281ddf8792a083709575cc685473a8d6a8207331
pradyun <pradyun.setti@gmail.com>
2021-01-25 13:57:21 +1300

    added menu bar functionality but uh very early stages rn

--- BarNote/BarNoteApp.swift
+++ BarNote/BarNoteApp.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 import CoreData
+import Foundation
 
 @main
 struct BarNoteApp: App {
     let persistenceController = PersistenceController.shared
-    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var popover = NSPopover.init()
+    var statusBarItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        let contentView = ContentView()
+
+        // Set the SwiftUI's ContentView to the Popover's ContentViewController
+//        popover.behavior = .transient !!! - This does not seem to work in SwiftUI2.0 or macOS BigSur yet
+        popover.animates = true
+        popover.contentViewController = NSViewController()
+        popover.contentViewController?.view = NSHostingView(rootView: contentView)
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusBarItem?.button?.title = "BN"
+        statusBarItem?.button?.action = #selector(AppDelegate.togglePopover(_:))
+    }
+    @objc func showPopover(_ sender: AnyObject?) {
+        if let button = statusBarItem?.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+//            !!! - displays the popover window with an offset in x in macOS BigSur.
+        }
+    }
+    @objc func closePopover(_ sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if popover.isShown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
         }
     }
 }
