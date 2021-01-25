9b5f49e7d9493703d1ac9dacba877da41c7631c2
pradyun <pradyun.setti@gmail.com>
2021-01-25 13:03:13 +1300

    Initial Commit

---
+++ BarNote/BarNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarNoteApp.swift
+//  BarNote
+//
+//  Created by Pradyun Setti on 25/01/21.
+//
+
+import SwiftUI
+
+@main
+struct BarNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarNote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarNote
+//
+//  Created by Pradyun Setti on 25/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
